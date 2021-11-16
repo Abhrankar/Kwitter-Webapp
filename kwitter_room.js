9c2d576c09f3510785d3645f9ed2deede5109740
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-11-16 16:05:04 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDW_y1WdOYUd3nB7KL6E-TjFvNUfB7Cono",
+  authDomain: "kwitter-webapp-2.firebaseapp.com",
+  databaseURL: "https://kwitter-webapp-2-default-rtdb.firebaseio.com",
+  projectId: "kwitter-webapp-2",
+  storageBucket: "kwitter-webapp-2.appspot.com",
+  messagingSenderId: "1088980831637",
+  appId: "1:1088980831637:web:38d2f808c72d5f93d13a9c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
